e3581381da30a0a0b792a981cece41c8090ad4a3
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-06-18 23:48:05 +0330

    prettifing

--- random_1/random1.f90
+++ random_1/random1.f90
@@ -11,39 +11,39 @@
 !	RANDOM NUMBER GENRATOR (First Problem)	!
 !---------------------------------------------!
 
-	PROGRAM random1
-	IMPLICIT NONE
+      PROGRAM random1
+         IMPLICIT NONE
 
-	CHARACTER*15		:: File_Name
-	INTEGER, PARAMETER	:: M=256
-	INTEGER				:: n, i, IBM, iseed
-	REAL, DIMENSION(M)	:: k
-	REAL, PARAMETER		:: 	nor=4.65661287E-10
+         CHARACTER*15		:: File_Name
+         INTEGER, PARAMETER	:: M=256
+         INTEGER				:: n, i, IBM, iseed
+         REAL, DIMENSION(M)	:: k
+         REAL, PARAMETER		:: 	nor=4.65661287E-10
 
-	File_Name="RAN1_DATA.TXT"
-	OPEN (17,FILE=File_Name)
+         File_Name="RAN1_DATA.TXT"
+         OPEN (17,FILE=File_Name)
 
-	PRINT *, 'Give number of Random number that you want ?'
-	READ *, n
-	PRINT *,'Give SEED number for genrating Random numbers ?'
-	READ *, iseed
-	
-	
-	IBM=2*iseed-1
+         PRINT *, 'Give number of Random number that you want ?'
+         READ *, n
+         PRINT *,'Give SEED number for genrating Random numbers ?'
+         READ *, iseed
 
-	DO i=1,n
-		IBM=IBM*16807
-		IF (IBM.LT.0) THEN
-			IBM=-IBM
-		END IF
-		k(i)=nor * REAL(IBM)
-	END DO
 
-	DO i=1,n
-		PRINT *,k(i)
-		WRITE (17,*) k(i)
-	END DO
+         IBM=2*iseed-1
 
-	CLOSE(17)
+         DO i=1,n
+            IBM=IBM*16807
+            IF (IBM.LT.0) THEN
+               IBM=-IBM
+            END IF
+            k(i)=nor * REAL(IBM)
+         END DO
 
-	END PROGRAM random1
+         DO i=1,n
+            PRINT *,k(i)
+            WRITE (17,*) k(i)
+         END DO
+
+         CLOSE(17)
+
+      END PROGRAM random1
